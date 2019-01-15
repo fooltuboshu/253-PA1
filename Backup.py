2f60e879a1c9a939f7ab24baf14f6e2edb2478e2
TallnWide <tarekjamal070@gmail.com>
2019-01-14 23:53:10 -0800

    softmax

--- Backup.py
+++ Backup.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 import re
 import random
 from dataloader import display_face, images, labels
-from sklearn.decomposition import PCA
+
+#removelater
+from sklearn import preprocessing
+
+# print(preprocessing.scale(A))
+# print(standardizeData(A))
 
 #get train, validation and test data
 
@@ -20,7 +25,6 @@ def oneHotEncoding(Y):
 	for i in range(len(Y)):
 		encodedY.append(np.zeros(numOfDistintcVals))
 		encodedY[i][oneHotEncodeVariables[Y[i]]] = 1
-	print(oneHotEncodeVariables)
 
 	return encodedY
 
@@ -34,41 +38,29 @@ def getRawMatrix(a): # a is image48
 
 	return dataMatrix
 
-from sklearn import preprocessing
-
-def applyPCA(d, k):
-
-	data = preprocessing.scale(data)
+def standardizeData(data):
 
 	dataT = data.T
-	# meanDataT = dataT.mean(axis=0).flatten()
-	# dataT -= meanDataT
-	U, S, VT = np.linalg.svd(dataT, full_matrices=False)
 
+	dataT = (dataT - np.mean(dataT, axis=0)) / np.std(dataT, axis=0)
+
+	return dataT.T
+
+
+def PCA(data, k):
+
+	dataT = data.T
+	U, S, VT = np.linalg.svd(dataT, full_matrices=False)
 	V = VT.T
 	V = V[:,:k]
-	S = S[:k]
 
 	PCs = dataT.dot(V)
 
-	for i in range(k):	
-		PCs[:, i] = PCs[:, i]/np.sum(PCs[:, i], axis = 0) #np.sqrt(S[i])*
-	print(PCs[0])
-	pca = PCA(n_components=k, whiten=True)
-	pca.fit(data)
-	dataNew = pca.transform(data)
-	print(np.mean(dataNew, axis=0))
-	print(np.std(dataNew, axis=0))
-	print("fwe", pca.components_.shape)
-	print("PCA singule", pca.singular_values_)
+	for i in range(k):		
+		PCs[:, i] = np.sqrt(S[i])*PCs[:,i]
+		PCs[:, i] = PCs[:, i]/np.linalg.norm(PCs[:, i], axis = 0)
 
-	print("MIND", S)
-	# #standardizing the data
-	# for i in range(dataT.shape[1]):
-	# 	dataT[:, i] = dataT[:, i]/np.sqrt(S[i])
-
-
-	return dataT.T.dot(PCs)
+	return PCs
 
 def cal_cost(theta, X, Y):
 	N = X.shape[0]
@@ -94,9 +86,9 @@ def BGD(X, Y, valX, valY):
 	N = X.shape[0]
 	d = X.shape[1]	
 	c = Y.shape[1]
-	alpha = 0.01
+	alpha = 0.000001
 	num_iteration = 50
-	cost = np.zeros((num_iteration, 1))
+	cost = np.zeros((num_iteration, 1))	
 	costVal = np.zeros((num_iteration, 1))
 	theta = np.zeros((d,c))
 
@@ -108,9 +100,9 @@ def BGD(X, Y, valX, valY):
 		cost[it] = cal_cost(theta, X, Y)
 		costVal[it] = cal_cost(theta, valX, valY)
 
-		if it > 0 and costVal[it] > costVal[it-1]:
-			print("early-stopping at ",it)
-			break 
+		# if it>0 and costVal[it] > costVal[it-1]:
+		# 	print(it)
+		# 	break
 		# print(theta)
 
 	return theta, cost, costVal
@@ -144,22 +136,21 @@ def splitData(images):
 
 	#encode the labels
 	Y = [x.split('_')[1][0] for x in Y]
-	encodedY = Y#oneHotEncoding(Y)
+	encodedY = oneHotEncoding(Y)
 
-	trainX = getRawMatrix([X[i] for i in trainIds])
+	trainX = (getRawMatrix([X[i] for i in trainIds]))
 	trainY = np.asarray([encodedY[i] for i in trainIds])
 
 
-	validationX = getRawMatrix([X[i] for i in validationIds])
+	validationX =  (getRawMatrix([X[i] for i in validationIds]))
 	validationY = np.asarray([encodedY[i] for i in validationIds])
 
 
-	testX = getRawMatrix([X[i] for i in testIds])
+	testX =  (getRawMatrix([X[i] for i in testIds]))
 	testY = np.asarray([encodedY[i] for i in testIds])
 
 
 	return trainX, trainY, validationX, validationY, testX, testY
-
 
 def getAccuracy(theta, testX, testY):
 
@@ -174,76 +165,43 @@ def getAccuracy(theta, testX, testY):
 			correct.append(1)
 		else:
 			correct.append(0)
-	print(len(correct))
-
 	return sum(correct)/len(correct)
 
-accuracies = []
 
-
-############### DEBUGGING PURPOSE ONLY #####################
-
-from sklearn.linear_model import LogisticRegression
-
-acc = []
-
+accuracy = []
 for i in range(10):
 	trainX, trainY, validationX, validationY, testX, testY = splitData(images)
 
-	mean = np.mean(trainX, axis = 0 )
-	std = np.std(trainX, axis = 0)
-	scaler = preprocessing.StandardScaler().fit(trainX)
+	# preprocess()
 
-	trainX = scaler.transform(trainX)
+	PCs= PCA(trainX, 20)
 
-	pca = PCA(n_components=30, whiten=True)
-	pca.fit(trainX)
-	trainXPCA = pca.transform(trainX)
+	mean = np.mean(trainX, axis=0)
+	std = np.std(trainX, axis=0)
+	trainX = (trainX - mean) / std
+	trainXPCA = trainX.dot(PCs)
 	trainXPCA =  np.c_[np.ones((trainXPCA.shape[0],1)),trainXPCA]
 
-
-	#validationX = (validationX-mean)/std
-	validationXPCA = pca.transform(scaler.transform(validationX))
+	
+	validationX = (validationX -mean) / std
+	validationXPCA = validationX.dot(PCs)
 	validationXPCA =  np.c_[np.ones((validationXPCA.shape[0],1)),validationXPCA]
 
-	#testX = (testX-mean)/std
-	testXPCA = pca.transform(scaler.transform(testX))
+	testX = (testX - mean) / std
+	testXPCA = testX.dot(PCs)
 	testXPCA =  np.c_[np.ones((testXPCA.shape[0],1)),testXPCA]
 
-	clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(trainXPCA, trainY)
-	acc.append(clf.score(testXPCA, testY))
+	theta, cost, costVal = BGD(trainXPCA, trainY, validationXPCA, validationY)
 
-print(np.mean(acc))
-print(np.std(acc))
-
-# for i in range(10):
-# 	trainX, trainY, validationX, validationY, testX, testY = splitData(images)
-
-# 	# preprocess()
-# 	mean = np.mean(trainX, axis = 0 )
-# 	std = np.std(trainX, axis = 0)
-# 	trainX = preprocessing.scale(trainX)
-
-# 	pca = PCA(n_components=30, whiten=True)
-# 	pca.fit(trainX)
-# 	trainXPCA = pca.transform(trainX)
-# 	trainXPCA =  np.c_[np.ones((trainXPCA.shape[0],1)),trainXPCA]
+	# print(cost)
+	# print(costVal)
 
 
-# 	validationX = (validationX-mean)/std
-# 	validationXPCA = pca.transform(validationX)
-# 	validationXPCA =  np.c_[np.ones((validationXPCA.shape[0],1)),validationXPCA]
+	accuracy.append(getAccuracy(theta, testXPCA, testY))
 
-# 	testX = (testX-mean)/std
-# 	testXPCA = pca.transform(testX)
-# 	testXPCA =  np.c_[np.ones((testXPCA.shape[0],1)),testXPCA]
-
-# 	theta, cost, costVal = BGD(trainXPCA, trainY, validationXPCA, validationY)
-	
-# 	accuracies.append(getAccuracy(theta, testXPCA, testY))
-
-# print(np.mean(accuracies))
-# print(np.std(accuracies))
+print(np.mean(accuracy))
+print(np.std(accuracy))
+# print(len(costVal))
 
 # # softmax regression
 # w = np.zeros([trainX.shape[1], trainY.shape[1]])
@@ -255,12 +213,12 @@ print(np.std(acc))
 #     loss,grad = getLoss(w,trainX,trainY,lamda)
 #     losses.append(loss)
 #     w = w - (learningRate * grad)
-# print(loss)
+# # print(loss)
 
 
 #display_face(reconMatrix[23].reshape(380,240))
 
-#for i in range(6):
+# #for i in range(6):
 
 # x = dataT.dot(V[0]) 
 
@@ -275,7 +233,7 @@ print(np.std(acc))
 
 
 
-# # print(X.shape[0])
+# # # print(X.shape[0])
 # X_b = np.c_[np.ones((len(X), 1)), X]
 
 
